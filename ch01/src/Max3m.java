6f2044d0dfa841eab834709aeed2b2cb7b08d93a
오세진 <jinioh88@gmail.com>
2018-08-19 14:09:32 +0900

    연습문제1

--- ch01/src/Max3m.java
+++ ch01/src/Max3m.java
@@ -6,6 +6,8 @@ public class Max3m {
 
         System.out.println("세 정수의 최댓값 구하기.");
         System.out.println(max(19,29,93));
+        System.out.println(max4(10,20,30,21));
+        System.out.println(min3(10,22,14));
     }
 
     static int max(int a, int b, int c) {
@@ -17,5 +19,32 @@ public class Max3m {
             max = c;
 
         return max;
+    }
+
+    static int max4(int a, int b, int c, int d) {
+        int max = a;
+        if(b>max)
+            max = b;
+        if(c>max)
+            max = c;
+        if(d>max)
+            max = d;
+        return max;
+    }
+
+    static int min3(int a, int b, int c) {
+        int min = a;
+        if(b<min)   min = b;
+        if(c<min)   min = c;
+        return min;
+    }
+
+    static int min4(int a, int b, int c, int d) {
+        int min = a;
+        if(b<min)   min = b;
+        if(c<min)   min = c;
+        if(d<min)   min = d;
+        
+        return min;
     }
 }
